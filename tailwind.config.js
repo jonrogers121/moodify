2a5f32d52c86a6174fca706f17f5e42b5609901a
Pablo Martínez <pablo.martinez94@proton.me>
2023-04-19 13:08:14 -0600

    feat(pages): home page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        spotify: {
+          green: '#1db954',
+          black: '#191414',
+        },
+      },
+      fontFamily: {
+        signika: 'var(--font-signika)',
+      },
     },
   },
   plugins: [],
