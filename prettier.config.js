9888d2104838fd436e8325af476475e7fa453ea1
Pablo Martínez <pablo.martinez94@proton.me>
2023-04-19 12:18:55 -0600

    build: commitlint, lint-staged prettier added; old next.js routing and ts-eslint plugin configured

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  jsxSingleQuote: false,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
